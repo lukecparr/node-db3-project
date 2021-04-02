93ecdd9e2a7dc32db15a043ee2abdaecc2f99b0f
Lukecp <lukecp@pm.me>
2021-04-01 22:35:31 -0400

    MVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -15,18 +15,25 @@ async function findById(scheme_id) { // EXERCISE B
     .where('sc.scheme_id', scheme_id)
     .orderBy('st.step_number', 'asc')
     .select('sc.scheme_name', 'sc.scheme_id', 'st.step_id', 'st.step_number', 'st.instructions');
+		
+	let formattedSteps = {};
+	console.log(steps);
 
-  const formattedSteps = {
-		scheme_id: steps[0].scheme_id,
-		scheme_name: steps[0].scheme_name,
-		steps: steps[0].step_id == null ? [] : steps.map(step => {
+	if (steps) {
+		formattedSteps = {
+			scheme_id: steps[0].scheme_id,
+			scheme_name: steps[0].scheme_name,
+			steps: steps[0].step_id == null ? [] : steps.map(step => {
 				return {
 					step_id: step.step_id,
 					step_number: step.step_number,
 					instructions: step.instructions,
 				};
-    }),
-  };
+			}),
+		}
+  } else {
+		return null;
+	}
   
   return formattedSteps;
 
@@ -48,13 +55,21 @@ async function findSteps(scheme_id) {
 	}
 }
 
-function add(scheme) { // EXERCISE D
+async function add(scheme) { // EXERCISE D
+	const id = await db('schemes').insert(scheme)
+
+	return db('schemes').where({ scheme_id: id }).first();
+
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) { // EXERCISE E
+	await db('steps').insert({ scheme_id: scheme_id, ...step});
+
+	return db('steps').where({ scheme_id: scheme_id}).orderBy('step_number', 'asc');
+
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
